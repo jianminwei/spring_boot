1822f7ffea43b1094d42ebc31663ae0e09122bdb
Jianmin Wei <jianminwei1@gmail.com>
2022-05-03 07:05:52 -0400

    added a CommandLineRunner to the Getting Started app

--- getting_started/src/main/java/com/maheshb/getting_started/GettingStartedApplication.java
+++ getting_started/src/main/java/com/maheshb/getting_started/GettingStartedApplication.java
@@ -1,7 +1,9 @@
 package com.maheshb.getting_started;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GettingStartedApplication {
@@ -10,4 +12,12 @@ public class GettingStartedApplication {
 		SpringApplication.run(GettingStartedApplication.class, args);
 	}
 
+	//This is another way to return a "run()" method for CommandLineRunner Bean.
+	//Spring Boot will run the lambda function returned.
+	@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+			System.out.println("You can put your codes here. Spring Boot will run it.");
+		};
+	}
 }
